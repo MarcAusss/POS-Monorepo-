e17a3b55ca816f466ec4197bc393f41064163a49
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-03-11 09:15:58 +0800

    asd

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// routes/api.php
+Route::apiResource('fruits', FruitController::class);
+Route::apiResource('transactions', TransactionController::class);
+Route::apiResource('reservations', ReservationController::class);
